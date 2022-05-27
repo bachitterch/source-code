55753897cb616b4be9129043b28d57fab96a9e40
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-05-27 02:52:04 -0700

    feat: added chat form(testing)

--- apps/twchat/src/pages/streams/[slug].tsx
+++ apps/twchat/src/pages/streams/[slug].tsx
@@ -1,6 +1,8 @@
 import getStreams from '@lib/getStreams'
+import React, { useState, useEffect, FormEvent } from 'react'
 import { NextPage } from 'next'
 import { getSession, useSession } from 'next-auth/react'
+import tmi, { Client } from 'tmi.js'
 
 type stream = {
   game_id: string
@@ -20,12 +22,157 @@ type stream = {
 }
 
 const Stream: NextPage = ({ streamdata }: any) => {
+  const [userData, setUserData] = useState<any>([])
+  const [msg, setMsg] = useState('')
+
   console.log(streamdata)
   const { data: session } = useSession()
+  const username = session?.user?.name
+  const token = session?.accessToken
+  const streamer = streamdata?.user_login
 
-  return <div>Stream</div>
+  const clientOptions = {
+    options: {
+      skipUpdatingEmotesets: true
+    },
+    connection: {
+      reconnect: true,
+      secure: true
+    },
+    identity: {
+      username: username,
+      password: 'oauth:' + token
+    },
+    channels: [streamer]
+  }
+
+  const client: Client = new tmi.Client(clientOptions)
+  client.connect()
+
+  useEffect(() => {
+    client.on('chat', (channel, tags, message, self) => {
+      if (self) return
+      const emotes = tags.emotes
+      const parsedMessages = parseMessage(message, emotes)
+      updateUserData(parsedMessages, tags)
+    })
+  }, [])
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    client.say(streamer, msg)
+    setMsg('')
+  }
+
+  const updateUserData = (message: string, tags: any) => {
+    return setUserData((prevstate: any) => [...prevstate, { message, tags }])
+  }
+
+  const timeoutUser = (username: string) => {
+    client.timeout(streamer, username, 300)
+  }
+
+  const deleteMessage = (id: string) => {
+    client.deletemessage(streamer, id)
+  }
+
+  const banUser = (username: string) => {
+    client.ban(streamer, username)
+  }
+
+  return (
+    <div>
+      <h1>Hello Next.js</h1>
+
+      {session && (
+        <div>
+          <div>
+            <small>Signed in as</small>
+            <br />
+            <p>{streamdata.user_name}</p>
+          </div>
+          <div>
+            {userData.map((user: any) => (
+              <div key={user.tags.id}>
+                {user.tags.mod ? (
+                  <div>
+                    <button
+                      onClick={() => {
+                        banUser(user.tags.username)
+                      }}
+                    >
+                      Ban
+                    </button>
+                    <button
+                      onClick={() => {
+                        timeoutUser(user.tags.username)
+                      }}
+                    >
+                      Timeout
+                    </button>
+                    <button
+                      onClick={() => {
+                        deleteMessage(user.tags.id)
+                      }}
+                    >
+                      Delete Message
+                    </button>
+                  </div>
+                ) : null}
+                <span>{user.tags['display-name']}</span>:{' '}
+                <span
+                  className='cb-user-message'
+                  dangerouslySetInnerHTML={{
+                    __html: user.message
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <input
+                type='text'
+                value={msg}
+                onChange={e => setMsg(e.target.value)}
+              />
+              <button type='submit'>Send</button>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 export default Stream
+
+const parseMessage = (message: string, emotes: any) => {
+  let newMessage = message.split('')
+
+  for (let emoteIndex in emotes) {
+    let emote = emotes[emoteIndex]
+
+    for (let charIndexes in emote) {
+      let emoteIndexes = emote[charIndexes]
+
+      if (typeof emoteIndexes == 'string') {
+        emoteIndexes = emoteIndexes.split('-')
+        emoteIndexes = [parseInt(emoteIndexes[0]), parseInt(emoteIndexes[1])]
+
+        for (let i = emoteIndexes[0]; i <= emoteIndexes[1]; ++i) {
+          newMessage[i] = ''
+        }
+
+        newMessage[emoteIndexes[0]] =
+          '<img width="25" class="emoticon" src="http://static-cdn.jtvnw.net/emoticons/v1/' +
+          emoteIndex +
+          '/3.0">'
+      }
+    }
+  }
+
+  return newMessage.join('')
+}
 
 export const getPaths = async (context: any) => {
   let paths: any[] = []
